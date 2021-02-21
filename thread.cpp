74fddd0190870508da8227fa627a7afb87ba7dc7
Mariusz Perzynski <m.perzyk@interia.pl>
2021-02-21 17:32:40 +0100

    second function with the same content

--- thread.cpp
+++ thread.cpp
@@ -26,6 +26,7 @@ using namespace chrono;
 auto start = chrono::system_clock::now();
 
 
+
 int letters(int a = 65){  //  literki alfabetu od a = 97 lub A = 65 mozna to zmienic w argumencie..
 
 cout << "Startowy watek: " << this_thread::get_id() << " ==> " << chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - start).count() << endl;
@@ -37,15 +38,32 @@ do {
     *out++ = i = rand() % 26 + a;
 } while(i != a + 25 || step < 15);
 
-
 return step;
 }
 
+void line(char z, unsigned n){ // funkcja wysweitla n znakow char z i przedstawia id watku..
+
+    cout << endl;
+    cout << endl;
+
+cout << "Startowy watek drugi: " << this_thread::get_id() << " ==> " << chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - start).count() << endl;
+
+while(n--){
+cout << z;
+this_thread::sleep_for(chrono::milliseconds(125));
+}
+
+cout << "Konczacy watek drugi: " << this_thread::get_id() << " ==> " << chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - start).count() << endl;
+
+}
 
 
 int main(){
+    char z;
+    unsigned n;
 
 letters();
+line(z,n);
 
 return 0;
 }
