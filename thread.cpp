b93b54bbcd685d9d6c582b1898f34c5968190e72
Mariusz Perzynski <m.perzyk@interia.pl>
2021-02-21 17:13:08 +0100

    letters from 65 to 97 and reverse function

--- thread.cpp
+++ thread.cpp
@@ -13,6 +13,7 @@
 #include <deque>
 #include <functional>
 #include <typeinfo>
+#include <iterator>
 #include <set>
 #include <chrono>
 #include <thread>
@@ -22,6 +23,29 @@
 using namespace std;
 using namespace chrono;
 
+auto start = chrono::system_clock::now();
+
+
+int letters(int a = 65){  //  literki alfabetu od a = 97 lub A = 65 mozna to zmienic w argumencie..
+
+cout << "Startowy watek: " << this_thread::get_id() << " ==> " << chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now() - start).count() << endl;
+int step = 0, i;
+ostream_iterator<char> out(std::cout, " ");
+
+do {
+    step++;
+    *out++ = i = rand() % 26 + a;
+} while(i != a + 25 || step < 15);
+
+
+return step;
+}
+
+
+
 int main(){
-    
+
+letters();
+
+return 0;
 }
